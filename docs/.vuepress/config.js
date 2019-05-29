56df54fd16ea101506e45a47aa46aea270f548c0
truejasonfans <472408570@qq.com>
2019-05-30 00:22:40 +0800

    1. parcel test note1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,19 +1,28 @@
 module.exports = {
-  title: '前端知识图谱',
-  description: 'front knowledge collecting repo.',
+  title: "前端知识图谱",
+  description: "front knowledge collecting repo.",
   themeConfig: {
-    repo: 'TRUEJASONFANS/front_doc_repo',
-    lastUpdated: 'Last Updated',
+    repo: "TRUEJASONFANS/front_doc_repo",
+    lastUpdated: "Last Updated",
     editLinks: true,
-    editLinkText: '在 github 上编辑此页',
-    docsDir: 'docs',
+    editLinkText: "在 github 上编辑此页",
+    docsDir: "docs",
     nav: [
-      { text: '指南', link: '/guide/' },
-      { text: 'API', link: '/api/' },
-      { text: '知识图谱', link: '/knowledgemap/' },
+      { text: "指南", link: "/guide/" },
+      { text: "API", link: "/api/" },
+      { text: "知识图谱", link: "/knowledgemap/" }
     ],
     sidebar: {
-      '/guide/': []
-    },
-  },
+      "/guide/": [
+        {
+          title: "指南",
+          collapsable: false,
+          children: [
+            "",
+            "parcel",
+          ]
+        }
+      ]
+    }
+  }
 };
