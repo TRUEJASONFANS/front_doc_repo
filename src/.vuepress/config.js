393bd5732c709eadc9f15023d82a7d8dd1250fad
truejasonfans <472408570@qq.com>
2019-08-06 01:02:41 +0800

    move the docs into src folder

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -2,14 +2,14 @@ module.exports = {
   title: "前端知识图谱",
   description: "front knowledge collecting repo.",
   port:"8080",
-  dest:"public",
+  dest:"docs",
   base:"/front_doc_repo/",
   themeConfig: {
     repo: "TRUEJASONFANS/front_doc_repo",
     lastUpdated: "Last Updated",
     editLinks: true,
     editLinkText: "在 github 上编辑此页",
-    docsDir: "docs",
+    docsDir: "src",
     nav: [
       { text: "指南", link: "/guide/" },
       { text: "API", link: "/api/" },
