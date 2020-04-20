d4bfb50da04bd987dca0b6cf10f7432619b74fcc
truejasonfans <472408570@qq.com>
2020-04-21 00:51:08 +0800

    init blog repo

---
+++ blog/.umirc.ts
@@ -0,0 +1,44 @@
+import { IConfig } from 'umi-types';
+
+// ref: https://umijs.org/config/
+const config: IConfig =  {
+  treeShaking: true,
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/BasicLayout',
+      routes: [
+        {
+          path: '/',
+          name: 'home',
+          component: './home/home',
+        },
+      ]
+    }
+  ],
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: { webpackChunkName: true },
+      title: 'blog',
+      dll: true,
+      locale: {
+        enable: true,
+        default: 'en-US',
+      },
+      routes: {
+        exclude: [
+          /models\//,
+          /services\//,
+          /model\.(t|j)sx?$/,
+          /service\.(t|j)sx?$/,
+          /components\//,
+        ],
+      },
+    }],
+  ],
+}
+
+export default config;
