fcf9f5ce3f0e9f9d356f3f6cd0a550f88c48ca3d
truejasonfans <472408570@qq.com>
2020-05-06 00:31:06 +0800

    add todo list app

---
+++ blog/src/pages/todos/components/TodoList.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+export default class TodoList extends React.Component {
+  render () {
+    var items = this.props.items.map((item, index) => {
+      return (
+        <TodoListItem key={index} item={item} index={index} removeItem={this.props.removeItem} markTodoDone={this.props.markTodoDone} />
+      );
+    });
+    return (
+      <ul className="list-group"> {items} </ul>
+    );
+  }
+}
+  
+class TodoListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onClickClose = this.onClickClose.bind(this);
+    this.onClickDone = this.onClickDone.bind(this);
+  }
+  onClickClose() {
+    var index = parseInt(this.props.index);
+    this.props.removeItem(index);
+  }
+  onClickDone() {
+    var index = parseInt(this.props.index);
+    this.props.markTodoDone(index);
+  }
+  render () {
+    var todoClass = this.props.item.done ? 
+        "done" : "undone";
+    return(
+      <li className="list-group-item ">
+        <div className={todoClass}>
+          <span className="glyphicon glyphicon-ok icon" aria-hidden="true" onClick={this.onClickDone}></span>
+          {this.props.item.value}
+          <button type="button" className="close" onClick={this.onClickClose}>&times;</button>
+        </div>
+      </li>     
+    );
+  }
+}
---
+++ blog/src/pages/todos/components/Todoform.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+export default class TodoForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  componentDidMount() {
+    this.refs.itemName.focus();
+  }
+  onSubmit(event) {
+    event.preventDefault();
+    var newItemValue = this.refs.itemName.value;
+    
+    if(newItemValue) {
+      this.props.addItem({newItemValue});
+      this.refs.form.reset();
+    }
+  }
+  render () {
+    return (
+      <form ref="form" onSubmit={this.onSubmit} className="form-inline">
+        <input type="text" ref="itemName" className="form-control" placeholder="add a new todo..."/>
+        <button type="submit" className="btn btn-default">Add</button> 
+      </form>
+    );   
+  }
+}
